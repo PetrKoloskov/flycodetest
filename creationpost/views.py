c0dea105fbb2f7a90dcafc630c3b8b6f2a5b6b00
PetrKoloskov <65534753+PetrKoloskov@users.noreply.github.com>
2020-07-25 10:24:54 +0300

    create base_generic.html and index.html

--- creationpost/views.py
+++ creationpost/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from .models import Post
+def index(request):
+    posts=Post.objects.all().count()
+    return render(
+        request,
+        'index.html',
+        context={'posts':posts}
+    )
 # Create your views here.
